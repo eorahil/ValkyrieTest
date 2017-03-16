53720ca31a21764db9132449d47ea75f5b651efa
David Castro <eorahil@hotmail.com>
2017-03-16 08:09:07 +0100

    iniRead unit tests file

---
+++ Content/IniReadTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assets.Scripts.Content
+{
+    [TestClass]
+    public class IniReadTest
+    {
+        static string currentPath;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            // Disble logger
+            ValkyrieDebug.enabled = false;
+            currentPath = System.IO.Path.GetFullPath(@"..\..\resources\testSample.ini");
+        }
+
+        [TestMethod]
+        public void TestLoadLocalIni()
+        {
+            IniData result = IniRead.ReadFromIni(currentPath);
+            Assert.IsNotNull(result);
+        }
+    }
+}
